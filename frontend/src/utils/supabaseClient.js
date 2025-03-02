62f767a3d57aa9a9c9179280544e61aed0eb8535
Akhil Anil <akhilanil.100@gmail.com>
2025-03-02 11:37:52 +0400

    Frontend changes

--- frontend/src/utils/supabaseClient.js
+++ frontend/src/utils/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://coghrwmmyyzmbnndlawi.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvZ2hyd21teXl6bWJubmRsYXdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY4NjAyMzIsImV4cCI6MjA1MjQzNjIzMn0.TqSt8BDML0yLvcJWIG-2bcF6PieMqAep3b_VTAkpHDs';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvZ2hyd21teXl6bWJubmRsYXdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4OTcyMjUsImV4cCI6MjA1NjQ3MzIyNX0.WLm0l2UeFPiPNxyClnM4bQpxw4TcYFxleTdc7K0G6AM';
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
